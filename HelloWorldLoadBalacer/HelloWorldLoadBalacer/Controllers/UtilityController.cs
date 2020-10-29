83a4f8597af59b397cc82e0c0678adf161fa5762
arnab_mohit <Khandokar.Arnab@brainstation23.com>
2020-10-29 14:59:17 +0600

    First commit

---
+++ HelloWorldLoadBalacer/HelloWorldLoadBalacer/Controllers/UtilityController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace HelloWorldLoadBalacer.Controllers
+{
+    [ApiController]
+    public class UtilityController : ControllerBase
+    {
+        private static readonly string[] Names = new[]
+        {
+            "Arnab", "Nesar", "Sumon"
+        };
+
+        
+        public UtilityController() {
+        }
+
+        [HttpGet("/GetData", Name = "GetData")]
+        public IEnumerable<string> GetData() {
+            return Names;
+        }
+
+        [HttpPost("/WriteData", Name = "WriteData")]
+        public string WriteData() {
+
+            //Add write code here
+
+            return "Write to DB sucessfull";
+        }
+    }
+}
