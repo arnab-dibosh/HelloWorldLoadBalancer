4b0d7360043473e6094fb49a4533add6d695e193
arnab_mohit <Khandokar.Arnab@brainstation23.com>
2021-01-13 01:16:24 +0600

    Added alert

--- TestHelloWorld/LoginApp/Controllers/DirectPayController.cs
+++ TestHelloWorld/LoginApp/Controllers/DirectPayController.cs
@@ -18,24 +18,28 @@ namespace IDTPApp.Controllers
             return View();
         }
         public  IActionResult DirectPay() {
+            try {
+                var objPayloadData = new Payload();
+                objPayloadData.xmlData = "SBL1BDDH,fahmid1@user.idtp,parvez@user.idtp,1.1,Online,DeviceID,01711000000,Dhaka,103.24.44.30,123456,E2Esblasd1231422, TIsblasd1231422, MSGIDsblasd1231422, REFSENBsblasd1231422";
 
-            var objPayloadData = new Payload();
-            objPayloadData.xmlData = "SBL1BDDH,fahmid1@user.idtp,parvez@user.idtp,1.1,Online,DeviceID,01711000000,Dhaka,103.24.44.30,123456,E2Esblasd1231422, TIsblasd1231422, MSGIDsblasd1231422, REFSENBsblasd1231422";
-
-            StringContent content = new StringContent(JsonConvert.SerializeObject(objPayloadData), Encoding.UTF8, "application/json");
+                StringContent content = new StringContent(JsonConvert.SerializeObject(objPayloadData), Encoding.UTF8, "application/json");
 
 
-            using var client = new HttpClient();
-            string endpoint = "http://localhost:7001" + "/TransactionInsertBaseLine";
-            using (HttpResponseMessage response = client.PostAsJsonAsync(endpoint, objPayloadData).Result)
-            {
+                using var client = new HttpClient();
+                string endpoint = "http://localhost:7001" + "/TransactionInsertBaseLine";
+                using (HttpResponseMessage response = client.PostAsJsonAsync(endpoint, objPayloadData).Result) {
 
-                response.EnsureSuccessStatusCode();
-                var result = response.Content.ReadAsStringAsync().Result;
-                ViewBag.Result = "Success";
-                return View("Result");
+                    response.EnsureSuccessStatusCode();
+                    var result = response.Content.ReadAsStringAsync().Result;
+                    ViewBag.Result = "Transaction Successfull.";
+                    
+                }
             }
+            catch (Exception ex) {
 
+                ViewBag.Result = ex.Message;
+            }
+            return View("Index");
         }
     }
 }
