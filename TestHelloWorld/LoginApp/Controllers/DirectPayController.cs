37cda72ea402579a8d593d41a4c6bd23763def1b
arnab_mohit <Khandokar.Arnab@brainstation23.com>
2021-01-12 19:13:20 +0600

    Added baseline api in helloworld

--- TestHelloWorld/LoginApp/Controllers/DirectPayController.cs
+++ TestHelloWorld/LoginApp/Controllers/DirectPayController.cs
@@ -1,16 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Helper.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace IDTPApp.Controllers
 {
     public class DirectPayController : Controller
     {
-        public IActionResult DirectPay() {
-            //API call here
-            return View();
+        public async void DirectPay() {
+
+            using (HttpClient client = new HttpClient()) {
+                StringContent content = new StringContent(JsonConvert.SerializeObject(new Payload()), Encoding.UTF8, "application/json");
+                
+                string endpoint = "localhost:3344" + "/TransactionFundInsertBaseLine";
+                using (var Response = await client.PostAsync(endpoint, content)) {
+                    if (Response.StatusCode == System.Net.HttpStatusCode.OK) {
+                   
+                    }
+                    else {
+                    }
+                }
+
+            }
+
         }
     }
 }
