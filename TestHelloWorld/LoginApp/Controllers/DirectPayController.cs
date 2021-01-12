b0b4245b58bd9d3f12622496d5091650680548f8
arnab_mohit <Khandokar.Arnab@brainstation23.com>
2021-01-12 18:45:45 +0600

    Added login app

---
+++ TestHelloWorld/LoginApp/Controllers/DirectPayController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IDTPApp.Controllers
+{
+    public class DirectPayController : Controller
+    {
+        public IActionResult DirectPay() {
+            //API call here
+            return View();
+        }
+    }
+}
