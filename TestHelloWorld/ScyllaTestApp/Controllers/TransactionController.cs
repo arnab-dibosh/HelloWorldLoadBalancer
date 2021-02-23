3d6ff6545ad5a0e049297ad526da522fd111214e
arnab_mohit <Khandokar.Arnab@brainstation23.com>
2021-02-23 14:55:54 +0600

    Added project for Scylla

---
+++ TestHelloWorld/ScyllaTestApp/Controllers/TransactionController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScyllaTestApp.Controllers
+{
+    [ApiController]
+    public class TransactionController : Controller
+    {
+        [HttpPost("/Transferfundcsvwithscylla", Name = "Transferfundcsvwithscylla")]
+        public string Transferfundcsvwithscylla( Payload payload) {
+            string apiRequestStartTime = "";
+            apiRequestStartTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fffffff");
+
+            try {
+
+                string strDockerName = System.Environment.MachineName;
+                string dtApiResponseTime = string.Empty;
+                try {
+                    
+                }
+                catch (Exception) {
+                    throw;
+                }
+
+                return "Insert Successfull";
+            }
+            catch (Exception) {
+                throw;
+            }
+        }
+
+    }
+
+    public class Payload
+    {
+        public string CsvData { get; set; }
+    }
+}
