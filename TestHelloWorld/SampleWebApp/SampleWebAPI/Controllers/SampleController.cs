c4e8be4766c911b2a4ea82058379ea7ccd79b0dc
arnab_mohit <Khandokar.Arnab@brainstation23.com>
2021-07-28 13:49:41 +0600

    New Hello Test App

---
+++ TestHelloWorld/SampleWebApp/SampleWebAPI/Controllers/SampleController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SampleController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public SampleController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost("/SampleInsert", Name = "SampleInsert")]
+        public IActionResult SampleInsert(dynamic Payload)
+        {
+            var isInserted = OneInsertDynamicSql(Payload.ToString());
+            return Ok(isInserted);
+        }
+
+        private bool OneInsertDynamicSql(string PortalServer)
+        {
+            try
+            {
+                string query = "INSERT INTO Sample" + "(Id,Message,APIServer,PortalServer,CreatedOn) " +
+                    "VALUES('" + Guid.NewGuid().ToString() + "', '" + "Success" + "', '" + System.Environment.MachineName +
+                    "', '" + PortalServer + "', GetDate() )";
+
+                using (SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("PrimaryDB")))
+                {
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        connection.Open();
+                        command.ExecuteNonQuery();
+                        connection.Close();
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return true;
+        }
+    }
+}
---
+++ TestHelloWorld/SampleWebApp/SampleWebPortal/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using SampleWebPortal.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace SampleWebPortal.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        private readonly ILogger<HomeController> _logger;
+        private static HttpClient client = new HttpClient();
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.MachineName = Environment.MachineName;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SampleInsert()
+        {
+            var APIURL = _configuration["SampleTestAPIURL"];
+            var machineName = Environment.MachineName;
+            ViewBag.MachineName = machineName;
+            try
+            {
+                client.PostAsJsonAsync(APIURL + "SampleInsert", machineName);
+                ViewBag.ReturnMessage = "Insert Successful";
+            }
+            catch (Exception ex)
+            {
+                ViewBag.ReturnMessage = ex.Message;
+            }
+
+            return View("Index");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
